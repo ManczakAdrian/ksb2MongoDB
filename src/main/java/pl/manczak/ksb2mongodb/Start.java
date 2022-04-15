c2881114e129e99bf26240904c0237f5466c2a6b
ManczakAdrian <manczakadrian@gmail.com>
2022-04-15 20:00:02 +0200

    Crud

--- src/main/java/pl/manczak/ksb2mongodb/Start.java
+++ src/main/java/pl/manczak/ksb2mongodb/Start.java
@@ -5,21 +5,36 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+
 @Component
 public class Start {
 
-private ToyRepo toyRepo;
+    private ToyRepo toyRepo;
 
     public Start(ToyRepo toyRepo) {
         this.toyRepo = toyRepo;
     }
 
-    @EventListener (ApplicationReadyEvent.class)
-    public void init(){
-        Toy toyTeddy=new Toy("Miś uszatek", ToyType.TEDDY_BEAR);
-        Toy toyDoll=new Toy("Lala", ToyType.TEDDY_BEAR);
-        toyRepo.save(toyTeddy);
-        toyRepo.save(toyDoll);
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        Toy toyTeddy = new Toy("Miś uszatek", ToyType.TEDDY_BEAR);
+        Toy toyDoll = new Toy("Lala", ToyType.TEDDY_BEAR);
+//        toyRepo.save(toyTeddy);
+//        toyRepo.save(toyDoll);
+
+
+        Toy toy = toyRepo.findById("6259ad8cb9450d71590cce7e").get();
+
+        toy.setToyType(ToyType.DOLL);
+        toyRepo.save(toy);
+
+
+      // toyRepo.delete(toy);
+
+       //toyRepo.deleteAllById(Collections.singleton("6259ad8cb9450d71590cce7e"));
+
+        toyRepo.findAll().forEach(System.out::println);
 
     }
 
--- src/main/java/pl/manczak/ksb2mongodb/Toy.java
+++ src/main/java/pl/manczak/ksb2mongodb/Toy.java
@@ -44,4 +44,13 @@ public class Toy {
     public void setToyType(ToyType toyType) {
         this.toyType = toyType;
     }
+
+    @Override
+    public String toString() {
+        return "Toy{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", toyType=" + toyType +
+                '}';
+    }
 }
